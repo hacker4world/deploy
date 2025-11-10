84c3cdbf5bdaf3cd99d93ebab449b6e14a898a07
hacker4world <zonegaming514@gmail.com>
2025-11-10 15:37:07 +0100

    ...

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -3,6 +3,6 @@ const mysql = require("mysql2");
 module.exports = mysql.createConnection({
   host: "localhost",
   user: "app_user",
-  password: "app_user",
+  password: "Aa&21794636",
   database: "deployment",
 });
